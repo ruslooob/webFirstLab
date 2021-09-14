f4474ab7e9110c1f32d45e6d6630dfa8138d2d55
ruslooob <rm952576@gmail.com>
2021-09-14 21:50:16 +0300

    выведение свайпера за пределы контейнера

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
   
+
     slidesPerView: 2,
+    spaceBetween: 20,
   
     // Navigation arrows
     navigation: {
